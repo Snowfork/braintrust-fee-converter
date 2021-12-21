7c0fb794606be673aa5ce91bfc5122ccc06b0f9a
StephKua <stephenkkz97@gmail.com>
2021-12-22 01:23:42 +0800

    resolve fixes

--- client/src/utils/shared.js
+++ client/src/utils/shared.js
@@ -1,4 +1,8 @@
+import Web3 from "web3";
+import { CONTRACT_ABI, USDC_ABI } from "./abi";
+
 const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDRESS;
+const USDC_ADDRESS = process.env.REACT_APP_USDC_ADDRESS;
 
 export const getERC20Balance = async (contract, address) => {
   return contract.methods.balanceOf(address).call();
@@ -14,4 +18,19 @@ export const getERC20Allowance = async (contract, owner) => {
 
 export const getERC20Decimal = async (contract) => {
   return await contract.methods.decimals().call();
+};
+
+export const getAmountOutMin = async (provider, amount, slippage, quotePrice) => {
+  const web3 = new Web3(provider);
+  const CONVERTER_CONTRACT = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+  const USDC_CONTRACT = new web3.eth.Contract(USDC_ABI, USDC_ADDRESS);
+  const decimals = await getERC20Decimal(USDC_CONTRACT)
+
+  const contractPoolFee = await CONVERTER_CONTRACT.methods.poolFee().call();
+  const poolFee = 1 - (contractPoolFee / 1000000);
+  const amountReal = new web3.utils.BN(amount * Math.pow(10, decimals));
+  const slipInPerc = (100 - slippage) / 100;
+  const amountOutMin = new web3.utils.BN(amountReal * quotePrice * slipInPerc * poolFee);
+
+  return { amountOutMin, amountReal };
 };
